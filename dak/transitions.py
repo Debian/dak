e7b604956c0fc743453ff141d267a66850937dde
Joerg Jaspert <joerg@debian.org>
2008-04-18 00:02:07 +0200

    Also check for invalid package list indentation

--- dak/transitions.py
+++ dak/transitions.py
@@ -23,7 +23,7 @@
 
 ################################################################################
 
-import os, pg, sys, time, errno, fcntl, tempfile, pwd
+import os, pg, sys, time, errno, fcntl, tempfile, pwd, re
 import apt_pkg
 import daklib.database
 import daklib.utils
@@ -33,6 +33,8 @@ import syck
 Cnf = None
 Options = None
 projectB = None
+
+re_broken_package = re.compile(r"[a-zA-Z]\w+\s+\-.*")
 
 ################################################################################
 
@@ -125,12 +127,17 @@ def load_transitions(trans_file):
                 if type(t[key]) != list:
                     print "ERROR: Unknown type %s for packages in transition %s." % (type(t[key]), test)
                     failure = True
-
+                if re_broken_package.match(key):
+                    # Someone had a space too much (or not enough), we have something looking like
+                    # "package1 - package2" now.
+                    print "ERROR: Invalid indentation of package list in transition %s, around package(s): %s" % (test, key)
+                    failure = True
                 try:
                     for package in t["packages"]:
                         if type(package) != str:
                             print "ERROR: Packages list contains invalid type %s (as %s) in transition %s" % (type(package), package, test)
                             failure = True
+                        if 
                 except TypeError:
                     # In case someone has an empty packages list
                     print "ERROR: No packages defined in transition %s" % (test)
