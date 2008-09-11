c6198d1f5ec4e4534bd80ccab277645ec513b52b
Joerg Jaspert <joerg@debian.org>
2008-09-11 23:35:59 +0200

    Pseudo packages
    Moved pseudo packages to the bts people. Now fetch them from there.
    
    Signed-off-by: Joerg Jaspert <joerg@debian.org>

---
+++ scripts/debian/update-pseudopackages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Fetches latest copy of pseudo-packages
+# Joerg Jaspert <joerg@debian.org>
+
+. vars
+
+cd ${scriptdir}/masterdir
+
+echo Updating archive version of pseudo-packages
+for file in maintainers description; do
+	wget -t2 -T20 -q -N http://bugs.debian.org/pseudopackages/pseudo-packages.${file} || echo "Some error occured with $i..."
+done
