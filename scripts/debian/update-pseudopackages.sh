d950d3b8ef5a38873c5d2a411757ab1d626af5c6
Joerg Jaspert <joerg@debian.org>
2008-09-12 20:18:10 +0200

    Typo fix
    s/i/file/, it really helps to use the right variable names...
    Thanks Otavio.
    
    Signed-off-by: Joerg Jaspert <joerg@debian.org>

--- scripts/debian/update-pseudopackages.sh
+++ scripts/debian/update-pseudopackages.sh
@@ -9,5 +9,5 @@ cd ${scriptdir}/masterdir
 
 echo Updating archive version of pseudo-packages
 for file in maintainers description; do
-	wget -t2 -T20 -q -N http://bugs.debian.org/pseudopackages/pseudo-packages.${file} || echo "Some error occured with $i..."
+	wget -t2 -T20 -q -N http://bugs.debian.org/pseudopackages/pseudo-packages.${file} || echo "Some error occured with $file..."
 done
