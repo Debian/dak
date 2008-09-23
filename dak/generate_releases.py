ab2b2a7d6bd2ebf97136f0cc2cb9f815db517418
Joerg Jaspert <joerg@debian.org>
2008-09-23 20:35:11 +0200

    Valid-Until
    Add a Valid-Until "header" to the generated Release files.
    Meaning "$receiver shouldn't trust this files after that date".
    Should be used by apt and similar tools to detect some kind of MITM attacks,
    see #499897 for more information.
    Currently set to "7 days from now".
    
    Signed-off-by: Joerg Jaspert <joerg@debian.org>

--- dak/generate_releases.py
+++ dak/generate_releases.py
@@ -218,6 +218,7 @@ def main ():
         if codename != "":
             out.write("Codename: %s\n" % (codename))
         out.write("Date: %s\n" % (time.strftime("%a, %d %b %Y %H:%M:%S UTC", time.gmtime(time.time()))))
+        out.write("Valid-Until: %s\n" % (time.strftime("%a, %d %b %Y %H:%M:%S UTC", time.gmtime(time.time()+7*24*60*60))))
         if notautomatic != "":
             out.write("NotAutomatic: %s\n" % (notautomatic))
         out.write("Architectures: %s\n" % (" ".join(filter(utils.real_arch, SuiteBlock.ValueList("Architectures")))))
