f8fd64bd68e6d06da33e0838d1e3475640e190d2
Joerg Jaspert <joerg@debian.org>
2009-01-26 23:15:04 +0100

    p-u
    re_default_answer was missing.
    
    Signed-off-by: Joerg Jaspert <joerg@debian.org>

--- dak/process_unchecked.py
+++ dak/process_unchecked.py
@@ -38,7 +38,7 @@ from daklib.dak_exceptions import *
 from daklib.regexes import re_valid_version, re_valid_pkg_name, re_changelog_versions, \
                            re_strip_revision, re_strip_srcver, re_spacestrip, \
                            re_isanum, re_no_epoch, re_no_revision, re_taint_free, \
-                           re_isadeb, re_extract_src_version, re_issource
+                           re_isadeb, re_extract_src_version, re_issource, re_default_answer
 
 from types import *
 
