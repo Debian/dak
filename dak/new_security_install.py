6e6022ab339328bb3a867a31c9b1fee58253cae5
Joerg Jaspert <joerg@debian.org>
2009-02-12 22:57:54 +0100

    security_install
    enable the package-file-map
    
    Signed-off-by: Joerg Jaspert <joerg@debian.org>

--- dak/new_security_install.py
+++ dak/new_security_install.py
@@ -474,6 +474,7 @@ def _do_Approve():
     print "Updating file lists for apt-ftparchive..."
     spawn("dak make-suite-file-list")
     print "Updating Packages and Sources files..."
+    spawn("dak make-pkg-file-mapping | bzip2 -9 > /org/security.debian.org/ftp/indices/package-file.map.bz2")
     spawn("apt-ftparchive generate %s" % (utils.which_apt_conf_file()))
     print "Updating Release files..."
     spawn("dak generate-releases")
