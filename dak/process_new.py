ba8309222c0cdbee0288eeda331af56f05ee4897
Joerg Jaspert <joerg@debian.org>
2009-04-15 23:11:47 +0200

    Lets try it with correct syntax
    Signed-off-by: Joerg Jaspert <joerg@debian.org>

--- dak/process_new.py
+++ dak/process_new.py
@@ -980,7 +980,7 @@ def end():
         sys.stderr.write("Accepted %d package %s, %s.\n" % (accept_count, sets, utils.size_type(int(accept_bytes))))
         Logger.log(["total",accept_count,accept_bytes])
 
-    if not Options["No-Action"] and not Options["Trainee"]::
+    if not Options["No-Action"] and not Options["Trainee"]:
         Logger.close()
 
 ################################################################################
