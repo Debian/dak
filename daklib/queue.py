52da315d25f3b59d375d1908ccad2a5db90833bc
Frank Lichtenheld <djpig@debian.org>
2009-10-26 19:42:33 +0000

    queue: Fix typo
    Signed-off-by: Frank Lichtenheld <djpig@debian.org>

--- daklib/queue.py
+++ daklib/queue.py
@@ -72,7 +72,7 @@ def get_type(f, session):
     """
     # Determine the type
     if f.has_key("dbtype"):
-        file_type = file["dbtype"]
+        file_type = f["dbtype"]
     elif f["type"] in [ "orig.tar.gz", "orig.tar.bz2", "tar.gz", "tar.bz2", "diff.gz", "diff.bz2", "dsc" ]:
         file_type = "dsc"
     else:
