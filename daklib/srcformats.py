46434f2a671e5382758899140fbbd0fca2fff9b2
Chris Lamb <lamby@debian.org>
2009-10-27 10:28:59 +0000

    Add regex to match .dsc "Format: lala" value and compile it on creation.
    This forces every format to have a regex too as it will blow up otherwise.
    
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- daklib/srcformats.py
+++ daklib/srcformats.py
@@ -1,3 +1,5 @@
+import re
+
 srcformats = []
 
 class SourceFormat(type):
@@ -5,13 +7,21 @@ class SourceFormat(type):
         klass = super(SourceFormat, cls).__new__(cls, name, bases, attrs)
         srcformats.append(klass)
 
+        klass.re_format = re.compile(klass.format)
+
         return klass
 
 class FormatOne(object):
     __metaclass__ = SourceFormat
 
+    format = r'1.0'
+
 class FormatThree(object):
     __metaclass__ = SourceFormat
 
+    format = r'3\.\d+ \(native\)'
+
 class FormatThreeQuilt(object):
     __metaclass__ = SourceFormat
+
+    format = r'3\.\d+ \(quilt\)'
