5b8bb3355c968a16618a3fe29af9de15f1f11b95
Frank Lichtenheld <djpig@debian.org>
2009-10-30 11:27:46 +0000

    process-upload: Add standard boilerplate code
    Signed-off-by: Frank Lichtenheld <djpig@debian.org>

--- dak/process_upload.py
+++ dak/process_upload.py
@@ -125,3 +125,138 @@ Checks Debian packages from Incoming
 ##       pu: create files for BTS
 ##       pu: create entry in queue_build
 ##       pu: check overrides
+import errno
+import fcntl
+import os
+import sys
+#from datetime import datetime
+import apt_pkg
+
+from daklib import daklog
+#from daklib.queue import *
+from daklib import utils
+from daklib.dbconn import *
+#from daklib.dak_exceptions import *
+#from daklib.regexes import re_default_answer, re_issource, re_fdnic
+from daklib.urgencylog import UrgencyLog
+from daklib.summarystats import SummaryStats
+from daklib.config import Config
+
+###############################################################################
+
+Options = None
+Logger = None
+
+###############################################################################
+
+def init():
+    global Options
+
+    # Initialize config and connection to db
+    cnf = Config()
+    DBConn()
+
+    Arguments = [('a',"automatic","Dinstall::Options::Automatic"),
+                 ('h',"help","Dinstall::Options::Help"),
+                 ('n',"no-action","Dinstall::Options::No-Action"),
+                 ('p',"no-lock", "Dinstall::Options::No-Lock"),
+                 ('s',"no-mail", "Dinstall::Options::No-Mail"),
+                 ('d',"directory", "Dinstall::Options::Directory", "HasArg")]
+
+    for i in ["automatic", "help", "no-action", "no-lock", "no-mail",
+              "version", "directory"]:
+        if not cnf.has_key("Dinstall::Options::%s" % (i)):
+            cnf["Dinstall::Options::%s" % (i)] = ""
+
+    changes_files = apt_pkg.ParseCommandLine(cnf.Cnf, Arguments, sys.argv)
+    Options = cnf.SubTree("Dinstall::Options")
+
+    if Options["Help"]:
+        usage()
+
+    # If we have a directory flag, use it to find our files
+    if cnf["Dinstall::Options::Directory"] != "":
+        # Note that we clobber the list of files we were given in this case
+        # so warn if the user has done both
+        if len(changes_files) > 0:
+            utils.warn("Directory provided so ignoring files given on command line")
+
+        changes_files = utils.get_changes_files(cnf["Dinstall::Options::Directory"])
+
+    return changes_files
+
+###############################################################################
+
+def usage (exit_code=0):
+    print """Usage: dak process-upload [OPTION]... [CHANGES]...
+  -a, --automatic           automatic run
+  -h, --help                show this help and exit.
+  -n, --no-action           don't do anything
+  -p, --no-lock             don't check lockfile !! for cron.daily only !!
+  -s, --no-mail             don't send any mail
+  -V, --version             display the version number and exit"""
+    sys.exit(exit_code)
+
+###############################################################################
+
+def main():
+    global Logger
+
+    cnf = Config()
+    summarystats = SummaryStats()
+    changes_files = init()
+    log_urgency = False
+    stable_queue = None
+
+    # -n/--dry-run invalidates some other options which would involve things happening
+    if Options["No-Action"]:
+        Options["Automatic"] = ""
+
+    # Check that we aren't going to clash with the daily cron job
+
+    # Check that we aren't going to clash with the daily cron job
+    if not Options["No-Action"] and os.path.exists("%s/daily.lock" % (cnf["Dir::Lock"])) and not Options["No-Lock"]:
+        utils.fubar("Archive maintenance in progress.  Try again later.")
+
+    # Obtain lock if not in no-action mode and initialize the log
+    if not Options["No-Action"]:
+        lock_fd = os.open(cnf["Dinstall::LockFile"], os.O_RDWR | os.O_CREAT)
+        try:
+            fcntl.lockf(lock_fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except IOError, e:
+            if errno.errorcode[e.errno] == 'EACCES' or errno.errorcode[e.errno] == 'EAGAIN':
+                utils.fubar("Couldn't obtain lock; assuming another 'dak process-upload' is already running.")
+            else:
+                raise
+        Logger = daklog.Logger(cnf, "process-upload")
+        if cnf.get("Dir::UrgencyLog"):
+            # Initialise UrgencyLog()
+            log_urgency = True
+            UrgencyLog()
+
+    # Sort the .changes files so that we process sourceful ones first
+    changes_files.sort(utils.changes_compare)
+
+    # Process the changes files
+    for changes_file in changes_files:
+        print "\n" + changes_file
+        session = DBConn().session()
+        session.close()
+
+    if summarystats.accept_count:
+        sets = "set"
+        if summarystats.accept_count > 1:
+            sets = "sets"
+        sys.stderr.write("Installed %d package %s, %s.\n" % (summarystats.accept_count, sets,
+                                                             utils.size_type(int(summarystats.accept_bytes))))
+        Logger.log(["total", summarystats.accept_count, summarystats.accept_bytes])
+
+    if not Options["No-Action"]:
+        Logger.close()
+        if log_urgency:
+            UrgencyLog().close()
+
+###############################################################################
+
+if __name__ == '__main__':
+    main()
