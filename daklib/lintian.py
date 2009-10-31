c91496e85403f61439ffce6326ddd8c32cad5751
Chris Lamb <lamby@debian.org>
2009-10-31 09:43:52 +0000

    Add a docstring for parse_lintian_output
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- daklib/lintian.py
+++ daklib/lintian.py
@@ -1,6 +1,13 @@
 from regexes import re_parse_lintian
 
 def parse_lintian_output(output):
+    """
+    Parses Lintian output and returns a generator with the data.
+
+    >>> list(parse_lintian_output('W: pkgname: some-tag path/to/file'))
+    [('W', 'pkgname', 'some-tag', 'path/to/file')]
+    """
+
     for line in output.split('\n'):
         m = re_parse_lintian.match(line)
         if m:
