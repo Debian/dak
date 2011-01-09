3f70dafbc130cbebfe6b0a814831b05121411d4f
Torsten Werner <twerner@debian.org>
2011-01-09 21:47:38 +0100

    implement first test with database access
    * Implement a new test class: DBDakTestCase in db_test.py.
    * First test: dbtest_fingerprint.py is prefixed with dbtest_ instead of
      test_ because we do not have a test database on franck (yet).
    
    Signed-off-by: Torsten Werner <twerner@debian.org>

---
+++ tests/db_test.py
@@ -0,0 +1,44 @@
+from base_test import DakTestCase, fixture
+
+from daklib.config import Config
+from daklib.dbconn import DBConn
+
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SADeprecationWarning
+
+import pickle
+import warnings
+
+# suppress some deprecation warnings in squeeze related to sqlalchemy
+warnings.filterwarnings('ignore', \
+    "The SQLAlchemy PostgreSQL dialect has been renamed from 'postgres' to 'postgresql'.*", \
+    SADeprecationWarning)
+
+class DBDakTestCase(DakTestCase):
+    def setUp(self):
+        cnf = Config()
+        if cnf["DB::Host"]:
+            # TCP/IP
+            connstr = "postgres://%s" % cnf["DB::Host"]
+            if cnf["DB::Port"] and cnf["DB::Port"] != "-1":
+                connstr += ":%s" % cnf["DB::Port"]
+            connstr += "/%s" % cnf["DB::Name"]
+        else:
+            # Unix Socket
+            connstr = "postgres:///%s" % cnf["DB::Name"]
+            if cnf["DB::Port"] and cnf["DB::Port"] != "-1":
+                connstr += "?port=%s" % cnf["DB::Port"]
+
+        pickle_file = open(fixture('db-metadata.pkl'), 'r')
+        self.metadata = pickle.load(pickle_file)
+        self.metadata.ddl_listeners = pickle.load(pickle_file)
+        pickle_file.close()
+        self.metadata.bind = create_engine(connstr)
+        self.metadata.create_all()
+        self.session = DBConn().session()
+
+    def tearDown(self):
+        #pass
+        self.session.close()
+        #self.metadata.drop_all()
+
