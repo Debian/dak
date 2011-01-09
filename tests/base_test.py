504a32446cc5263a934aa815494356a7c38b4b13
Torsten Werner <twerner@debian.org>
2011-01-09 15:05:13 +0100

    suppress warnings when running tests in squeeze
    Signed-off-by: Torsten Werner <twerner@debian.org>

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,10 +1,26 @@
 import os
 import sys
 import unittest
+import warnings
 
 from os.path import abspath, dirname, join
 
 DAK_ROOT_DIR = dirname(dirname(abspath(__file__)))
+
+# suppress some deprecation warnings in squeeze related to apt_pkg,
+# debian, and md5 modules
+warnings.filterwarnings('ignore', \
+    "Attribute '.*' of the 'apt_pkg\.Configuration' object is deprecated, use '.*' instead\.", \
+    DeprecationWarning)
+warnings.filterwarnings('ignore', \
+    "apt_pkg\.newConfiguration\(\) is deprecated\. Use apt_pkg\.Configuration\(\) instead\.", \
+    DeprecationWarning)
+warnings.filterwarnings('ignore', \
+    "please use 'debian' instead of 'debian_bundle'", \
+    DeprecationWarning)
+warnings.filterwarnings('ignore', \
+    "the md5 module is deprecated; use hashlib instead", \
+    DeprecationWarning)
 
 class DakTestCase(unittest.TestCase):
     def setUp(self):
