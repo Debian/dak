c35377bd1fb884034708f74511f16aa06fca1d8a
Joerg Jaspert <joerg@debian.org>
2011-02-16 23:23:01 +0100

    export changelogs on security
    Signed-off-by: Joerg Jaspert <joerg@debian.org>

---
+++ config/debian-security/export.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -u
+set -E
+
+export SCRIPTVARS=/srv/security-master.debian.org/dak/config/debian-security/vars
+. $SCRIPTVARS
+
+# Make sure we start out with a sane umask setting
+umask 022
+
+# And use one locale, no matter what the caller has set
+export LANG=C
+export LC_ALL=C
+
+# extract changelogs and stuff
+function changelogs() {
+    log "Extracting changelogs"
+    dak make-changelog -e
+    mkdir -p ${exportpublic}/changelogs
+    cd ${exportpublic}/changelogs
+    rsync -aHW --delete --delete-after --ignore-errors ${exportdir}/changelogs/. .
+    sudo -H -u archvsync /home/archvsync/runmirrors metasdo > ~dak/runmirrors-metadata.log 2>&1 &
+}
+
+changelogs
