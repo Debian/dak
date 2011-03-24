c70c74dc4e298a7dd2cef8d11d82e1a9d281546d
Mark Hymers <mhy@debian.org>
2011-03-24 18:25:21 +0000

    New g-r command
    Signed-off-by: Mark Hymers <mhy@debian.org>

--- daklib/regexes.py
+++ daklib/regexes.py
@@ -126,5 +126,5 @@ re_parse_lintian = re.compile(r"^(?P<level>W|E|O): (?P<package>.*?): (?P<tag>[^ 
 re_match_expired = re.compile(r"^The key used to sign .+ has expired on .+$")
 
 # in generate-releases
-re_getsarelease = re.compile (r".*/(binary-[0-9a-z-]+|source)$")
-re_includeinarelease = re.compile (r"(Contents-[0-9a-z-]+.gz|Index|Packages(.gz|.bz2)?|Sources(.gz|.bz2)?|MD5SUMS|Release)$")
+re_gensubrelease = re.compile (r".*/(binary-[0-9a-z-]+|source)$")
+re_includeinrelease = re.compile (r"(Contents-[0-9a-z-]+.gz|Index|Packages(.gz|.bz2)?|Sources(.gz|.bz2)?|MD5SUMS|Release)$")
