38a69fc67dfe8d632d0c86dc4c9613affd6c9a0d
Mark Hymers <mhy@debian.org>
2011-03-26 22:12:38 +0000

    Add self. to rejects
    Signed-off-by: Mark Hymers <mhy@debian.org>

--- daklib/queue.py
+++ daklib/queue.py
@@ -1264,7 +1264,7 @@ class Upload(object):
 
         session.close()
 
-        return (len(rejects) == 0)
+        return (len(self.rejects) == 0)
 
     ###########################################################################
 
