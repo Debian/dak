e381846f897a42d36325f9a5ed1f3432caebe046
Mark Hymers <mhy@debian.org>
2011-07-27 20:18:53 +0100

    Use database instead of config file for Archive::
    Leave in security and bpo configs for now as they need
    upgrading before they're removed.
    
    Signed-off-by: Mark Hymers <mhy@debian.org>

--- dak/rm.py
+++ dak/rm.py
@@ -630,10 +630,17 @@ def main ():
         summarymail = "%s\n------------------- Reason -------------------\n%s\n" % (summary, Options["Reason"])
         summarymail += "----------------------------------------------\n"
         Subst_close_rm["__SUMMARY__"] = summarymail
+
         whereami = utils.where_am_i()
-        Archive = cnf.SubTree("Archive::%s" % (whereami))
-        Subst_close_rm["__MASTER_ARCHIVE__"] = Archive["OriginServer"]
-        Subst_close_rm["__PRIMARY_MIRROR__"] = Archive["PrimaryMirror"]
+        Archive = get_archive(whereami, session)
+        if Archive is None:
+            utils.warn("Cannot find archive %s.  Setting blank values for origin" % whereami)
+            Subst_close_rm["__MASTER_ARCHIVE__"] = ""
+            Subst_close_rm["__PRIMARY_MIRROR__"] = ""
+        else:
+            Subst_close_rm["__MASTER_ARCHIVE__"] = Archive.origin_server
+            Subst_close_rm["__PRIMARY_MIRROR__"] = Archive.primary_mirror
+
         for bug in utils.split_args(Options["Done"]):
             Subst_close_rm["__BUG_NUMBER__"] = bug
             if Options["Do-Close"]:
