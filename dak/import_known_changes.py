af59022f5398b4199bc4f60569ef2dcd35f433dc
Mark Hymers <mhy@debian.org>
2011-07-29 14:17:07 +0100

    Add missing import
    Signed-off-by: Mark Hymers <mhy@debian.org>

--- dak/import_known_changes.py
+++ dak/import_known_changes.py
@@ -32,7 +32,7 @@ import sys
 import os
 import logging
 import threading
-from daklib.dbconn import DBConn, get_dbchange
+from daklib.dbconn import DBConn, get_dbchange, get_known_changes
 from daklib.config import Config
 import apt_pkg
 from daklib.dak_exceptions import DBUpdateError, InvalidDscError, ChangesUnicodeError
