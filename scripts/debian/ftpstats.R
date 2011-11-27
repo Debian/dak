031b25dd5d828d29d268b773564886460fc8dca7
Mark Hymers <mhy@debian.org>
2011-11-27 20:57:01 +0000

    Show legend in 2 cols so it all gets showed
    Signed-off-by: Mark Hymers <mhy@debian.org>

--- scripts/debian/ftpstats.R
+++ scripts/debian/ftpstats.R
@@ -9,7 +9,7 @@ plotsize <- function(file,title,p,height=11.8,width=16.9) {
 	barplot(t(p),col = 1:19, main=title,
 		xlab="date", ylab="size (MiB)")
 	legend(par("usr")[1]+xinch(5),par("usr")[4]-yinch(0.1),legend=colnames(t),
-		ncol=3,fill=1:19,xjust=1,yjust=1)
+		ncol=2,fill=1:19,xjust=1,yjust=1)
 }
 t <- (read.table("/srv/ftp-master.debian.org/misc/ftpstats.data",sep=",",header=0,row.names=1,col.names=cname))/1024/1024
 v <- t[(length(t$all)-90):(length(t$all)),1:19]
