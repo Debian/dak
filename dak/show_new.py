Debian/dak

commit 24cb4fe2729e5805e8a9d81149dce947787ee20d

dak/show_new.py: setup worker pool before connecting to database

dak/show_new.py
@@ -245,13 +245,14 @@ def init(session):
 ################################################################################
 
 def main():
+    pool = DakProcessPool(processes=5)
+
     session = DBConn().session()
     upload_ids = [ u.id for u in init(session) ]
     session.close()
 
     examine_package.use_html=1
 
-    pool = DakProcessPool(processes=5)
     p = pool.map_async(do_pkg, upload_ids)
     pool.close()
 

commit 20181b6201b07b4b590280fcf9adea4f62ed8396

dak/show_new.py: set examine_package.use_html before creating worker pool
We want examine_package.use_html to be set everywhere. So we have to
change it before we create the worker pool.

This was broken by 24cb4fe2729e5805e8a9d81149dce947787ee20d where the
worker pool was setup earlier.

dak/show_new.py
@@ -249,13 +249,12 @@ def init(session):
 ################################################################################
 
 def main():
+    examine_package.use_html = True
     pool = DakProcessPool(processes=5)
 
     session = DBConn().session()
     upload_ids = [ u.id for u in init(session) ]
     session.close()
-
-    examine_package.use_html=1
 
     p = pool.map_async(do_pkg, upload_ids)
     pool.close()