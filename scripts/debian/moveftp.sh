6ecada6a3f9c26b505be4059d234c9e41639485e
Joerg Jaspert <joerg@debian.org>
2014-07-27 21:37:09 +0200

    We are in srv now, no longer org
    Signed-off-by: Joerg Jaspert <joerg@debian.org>

--- scripts/debian/moveftp.sh
+++ scripts/debian/moveftp.sh
@@ -3,7 +3,7 @@
 set -e
 set -u
 
-FTPDIR="/org/upload.debian.org/ftp/pub/UploadQueue/"
-SSHDIR="/org/upload.debian.org/UploadQueue/"
+FTPDIR="/srv/upload.debian.org/ftp/pub/UploadQueue/"
+SSHDIR="/srv/upload.debian.org/UploadQueue/"
 
 yes n | find ${FTPDIR} -type f -mmin +15 -print0 -exec mv -i --target-directory=${SSHDIR} "{}" +
