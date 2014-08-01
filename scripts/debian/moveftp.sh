36d3848729493bb72e4d672154a0c63e0256caf5
Ansgar Burchardt <ansgar@debian.org>
2014-08-01 18:21:09 +0200

    scripts/debian/moveftp.sh: use mv --no-clobber

--- scripts/debian/moveftp.sh
+++ scripts/debian/moveftp.sh
@@ -6,4 +6,4 @@ set -u
 FTPDIR="/srv/upload.debian.org/ftp/pub/UploadQueue/"
 SSHDIR="/srv/upload.debian.org/UploadQueue/"
 
-yes n | find ${FTPDIR} -type f -mmin +15 -print0 -exec mv -i --target-directory=${SSHDIR} "{}" +
+find ${FTPDIR} -type f -mmin +15 -print0 -exec mv --no-clobber --target-directory=${SSHDIR} -- "{}" +
