677e507613f0c410ffaefda67413ad82c2ed454c
Ansgar Burchardt <ansgar@debian.org>
2014-08-03 17:04:41 +0200

    Drop unique .changes name requirement and allow ftpteam to forget seen files.

---
+++ dak/dakdb/update103.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding=utf8
+
+"""
+Drop unique .changes name requirement and allow ftpteam to forget seen files
+
+@contact: Debian FTP Master <ftpmaster@debian.org>
+@copyright: 2014, Ansgar Burchardt <ansgar@debian.org>
+@license: GNU General Public License version 2 or later
+"""
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+################################################################################
+
+import psycopg2
+from daklib.dak_exceptions import DBUpdateError
+from daklib.config import Config
+
+statements = [
+"ALTER TABLE changes DROP CONSTRAINT IF EXISTS known_changes_changesname_key",
+"DROP INDEX IF EXISTS changesurgency_ind",
+"GRANT DELETE ON signature_history TO ftpteam",
+]
+
+################################################################################
+def do_update(self):
+    print __doc__
+    try:
+        cnf = Config()
+
+        c = self.db.cursor()
+
+        for stmt in statements:
+            c.execute(stmt)
+
+        c.execute("UPDATE config SET value = '103' WHERE name = 'db_revision'")
+        self.db.commit()
+
+    except psycopg2.ProgrammingError as msg:
+        self.db.rollback()
+        raise DBUpdateError('Unable to apply sick update 103, rollback issued. Error message: {0}'.format(msg))
