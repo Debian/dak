d47da55f3658cae8230bc960b8dd497eaf6330e9
Ansgar Burchardt <ansgar@debian.org>
2015-05-16 14:03:59 +0200

    Add --ca-directory=/etc/ssl/ca-debian to wget options.

--- scripts/debian/update-pseudopackages.sh
+++ scripts/debian/update-pseudopackages.sh
@@ -12,5 +12,5 @@ cd ${scriptdir}/masterfiles
 
 echo Updating archive version of pseudo-packages
 for file in maintainers description; do
-	wget -t2 -T20 -q -N https://bugs.debian.org/pseudopackages/pseudo-packages.${file} || echo "Some error occured with $file..."
+	wget -t2 -T20 -q -N --ca-directory=/etc/ssl/ca-debian https://bugs.debian.org/pseudopackages/pseudo-packages.${file} || echo "Some error occured with $file..."
 done
