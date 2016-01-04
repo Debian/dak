f99274b09b7fe45607d95d24fd83f2843307cd7d
Joerg Jaspert <joerg@debian.org>
2016-01-04 22:17:13 +0100

    Link "master" to the tracefile, to have one name only

--- config/debian-security/make-mirror.sh
+++ config/debian-security/make-mirror.sh
@@ -9,5 +9,7 @@ echo "Regenerating \"public\" mirror/ hardlink fun"
 date -u > /srv/security-master.debian.org/ftp/project/trace/security-master.debian.org
 echo "Using dak v1" >> /srv/security-master.debian.org/ftp/project/trace/security-master.debian.org
 echo "Running on host: $(hostname -f)" >> /srv/security-master.debian.org/ftp/project/trace/security-master.debian.org
+cd /srv/security-master.debian.org/ftp/project/trace/
+ln -sf security-master.debian.org master
 cd /srv/security.debian.org/archive/debian-security/
 rsync -aH --link-dest /srv/security-master.debian.org/ftp/ --exclude Archive_Maintenance_In_Progress --delete --delete-after --ignore-errors /srv/security-master.debian.org/ftp/. .
