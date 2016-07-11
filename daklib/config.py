1e66e0b557f3a8d2b575721a6e44ee6d7639b56c
Carlos Manuel Duclos Vergara <carlosduclosv@yahoo.com>
2016-07-11 19:57:56 +0200

    Changing from gethostbyaddr to gethostbyname
    gethostname() was being used inside gethostbyaddr(). This is most likely to
    fail, since gethostname() will return a host name not an ip address.
    
    Changing it to gethostbyname() so the result of gethostname() can be used directly.
    
    Signed-off-by: Carlos Manuel Duclos Vergara <carlosduclosv@yahoo.com>

--- daklib/config.py
+++ daklib/config.py
@@ -72,7 +72,7 @@ class Config(object):
 
         # Check whether our dak.conf was the real one or
         # just a pointer to our main one
-        res = socket.gethostbyaddr(socket.gethostname())
+        res = socket.gethostbyname(socket.gethostname())
         conffile = self.Cnf.get("Config::" + res[0] + "::DakConfig")
         if conffile:
             apt_pkg.read_config_file_isc(self.Cnf, conffile)
