e8acb4d8c89eebed5987b8c586dfa2b0a9aa4193
Joerg Jaspert <joerg@debian.org>
2016-10-23 13:34:27 +0200

    Add config for usper, change moveftp to deal with usper

--- scripts/debian/moveftp.sh
+++ scripts/debian/moveftp.sh
@@ -5,5 +5,13 @@ set -u
 
 FTPDIR="/srv/upload.debian.org/ftp/pub/UploadQueue/"
 SSHDIR="/srv/upload.debian.org/UploadQueue/"
+HOST=$(hostname -s)
 
-find ${FTPDIR} -type f -mmin +15 -print0 -exec mv --no-clobber --target-directory=${SSHDIR} -- "{}" +
+if [[ ${HOST} == coccia ]]; then
+    find ${FTPDIR} -type f -mmin +15 -print0 -exec mv --no-clobber --target-directory=${SSHDIR} -- "{}" +
+elif [[ ${HOST} == usper ]]; then
+    find ${FTPDIR} -maxdepth 1 -type f -mmin +15 -print0 -exec mv --no-clobber --target-directory=${SSHDIR} -- "{}" +
+    for defdir in {1..15}; do
+        find ${FTPDIR}/DELAYED/${defdir}-day -maxdepth 1 -type f -mmin +15 -print0 -exec mv --no-clobber --target-directory=${SSHDIR}/DELAYED/${defdir}-day -- "{}" +
+    done
+fi
