c2295fbe69e971d68d91a8cb446d8ba3ee2091b2
Joerg Jaspert <joerg@debian.org>
2016-10-25 22:36:21 +0200

    Merge into a third dir, fetch from there.
    avoids trouble with rsync partial names / slow uploads / ...

--- scripts/debian/moveftp.sh
+++ scripts/debian/moveftp.sh
@@ -8,14 +8,26 @@ FTPDIR="/srv/upload.debian.org/ftp/pub/UploadQueue/"
 SSHDIR="/srv/upload.debian.org/UploadQueue/"
 HOST=$(hostname -s)
 
+# For usper, targetdir is the sshdir, everywhere else, its a seperate
+# one, so we avoid fetching partial uploads from their sshdir.
+if [[ ${HOST} == usper ]]; then
+    TARGETDIR="${SSHDIR}"
+    TOPROCESS="${FTPDIR}"
+else
+    TARGETDIR="/srv/upload.debian.org/mergedtree"
+    TOPROCESS="${FTPDIR} ${SSHDIR}"
+fi
+
 # This runs on all queue hosts - merge ftp and ssh together. (The queued on usper only processes
-# the SSHDIR)
-find ${FTPDIR} -maxdepth 1 -type f -mmin +5 -print0 -exec mv --no-clobber --target-directory=${SSHDIR} -- "{}" +
-for defdir in {0..15}; do
-    find ${FTPDIR}/DELAYED/${defdir}-day -maxdepth 1 -type f -mmin +5 -print0 -exec mv --no-clobber --target-directory=${SSHDIR}/DELAYED/${defdir}-day -- "{}" +
+# the TARGETDIR)
+for sourcedir in ${TOPROCESS}; do
+    find ${sourcedir} -maxdepth 1 -type f -mmin +5 -print0 -exec mv --no-clobber --target-directory=${TARGETDIR} -- "{}" +
+    for defdir in {0..15}; do
+        find ${sourcedir}/DELAYED/${defdir}-day -maxdepth 1 -type f -mmin +5 -print0 -exec mv --no-clobber --target-directory=${TARGETDIR}/DELAYED/${defdir}-day -- "{}" +
+    done
 done
 
-# If we are the master host, we have a little extrawork to do, collect all
+# If we are the master host, we have a little extra work to do, collect all
 # files from other queue hosts.
 if [[ ${HOST} == usper ]]; then
     # And now fetch all files from the queue hosts
