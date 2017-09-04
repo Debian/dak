c372d743a9153a805fcc37eb8f01e86dd93621ff
Luca Falavigna <dktrkranz@debian.org>
2017-09-04 16:22:38 +0000

    process-new: automatically convert priority extra to optional
    Priority "extra" is deprecated, packages using that priority should
    be given priority "optional" instead. Given most packages are not
    catching up with recent policy update, enforce that while adding
    overrides upon NEW processing.
    
    Signed-off-by: Luca Falavigna <dktrkranz@debian.org>

--- dak/process_new.py
+++ dak/process_new.py
@@ -526,6 +526,12 @@ def do_new(upload, upload_copy, handler, session):
         if missing:
             print "NEW\n"
 
+        for i, package in enumerate(missing):
+            if package["type"] == "deb":
+                if package["priority"] == "extra":
+                    package["priority"] = "optional"
+                    missing[i] = package
+
         answer = "XXX"
         if Options["No-Action"] or Options["Automatic"]:
             answer = 'S'
