c337d5d47d0218e604ffe98f95c40792ddd2cd40
Joerg Jaspert <joerg@debian.org>
2018-01-11 10:32:58 +0100

    Ignore .nobackup files

--- scripts/debian/link_morgue.sh
+++ scripts/debian/link_morgue.sh
@@ -85,8 +85,9 @@ cd "${PROCESSDIR}"
 log "Processing ${PROCESSDIR}"
 find ${PROCESSDIR} -type f |
 while read mfile; do
-    if [[ -e ${mfile}.nosnapshot ]]; then
+    if [[ -e ${mfile}.nosnapshot ]] || [[ ${mfile} == .nobackup ]]; then
         # We know this file does not exist on snapshot, don't check again
+        # Also ignore .nobackup files
         continue
     fi
 
