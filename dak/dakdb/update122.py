a5565b096673bd7b48ca8b85fe89d18882a436ed
Ansgar Burchardt <ansgar@debian.org>
2019-07-06 20:44:05 +0200

    update122: update default settings for suites

---
+++ dak/dakdb/update122.py
@@ -0,0 +1,54 @@
+# coding=utf8
+"""
+Update default settings for suites
+
+@contact: Debian FTP Master <ftpmaster@debian.org>
+@copyright: 2019, Ansgar Burchardt <ansgar@debian.org>
+@license: GNU General Public License version 2 or later
+"""
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+################################################################################
+
+from __future__ import print_function
+
+import psycopg2
+from daklib.dak_exceptions import DBUpdateError
+
+################################################################################
+
+
+def do_update(self):
+    """
+    Update default settings for suites
+    """
+    print(__doc__)
+    try:
+        c = self.db.cursor()
+
+        c.execute("""
+          ALTER TABLE suite
+            ALTER COLUMN indices_compression SET DEFAULT ARRAY['xz'],
+            ALTER COLUMN i18n_compression SET DEFAULT ARRAY['xz'],
+            ALTER COLUMN byhash SET DEFAULT TRUE
+        """)
+
+        c.execute("UPDATE config SET value = '122' WHERE name = 'db_revision'")
+        self.db.commit()
+
+    except psycopg2.ProgrammingError as msg:
+        self.db.rollback()
+        raise DBUpdateError('Unable to apply sick update 122, rollback issued. Error message : %s' % (str(msg)))
