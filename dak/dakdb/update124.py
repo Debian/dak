7b9e39fdae137464bc4dc6bbdc46218b5399dbf8
Ansgar <ansgar@debian.org>
2020-10-28 17:09:56 +0100

    update124: do not use string interpolation to pass parameters to SQL
    Some descriptions also included a single quote which made string
    interpolation fail.

--- dak/dakdb/update124.py
+++ dak/dakdb/update124.py
@@ -109,9 +109,9 @@ def do_update(self):
         """)
 
         for section in sections:
-            c.execute("UPDATE section SET description='{1}', longdesc='{2}' WHERE section='{0}'".format(section, sections[section]["desc"], sections[section]["longdesc"]))
-            c.execute("UPDATE section SET description='{1}', longdesc='{2}' WHERE section='contrib/{0}'".format(section, sections[section]["desc"], sections[section]["longdesc"]))
-            c.execute("UPDATE section SET description='{1}', longdesc='{2}' WHERE section='non-free/{0}'".format(section, sections[section]["desc"], sections[section]["longdesc"]))
+            c.execute("UPDATE section SET description=%s, longdesc=%s WHERE section=%s", (section, sections[section]["desc"], sections[section]["longdesc"]))
+            c.execute("UPDATE section SET description=%s, longdesc=%s WHERE section=CONCAT('contrib/', %s)", (section, sections[section]["desc"], sections[section]["longdesc"]))
+            c.execute("UPDATE section SET description=%s, longdesc=%s WHERE section=CONCAT('non-free/', %s)", (section, sections[section]["desc"], sections[section]["longdesc"]))
 
         c.execute("UPDATE config SET value = '124' WHERE name = 'db_revision'")
         self.db.commit()
