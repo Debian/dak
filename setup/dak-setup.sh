46a8e273089c94e9110cc19e1c6be058c2924bea
Ansgar <ansgar@debian.org>
2020-10-28 17:03:21 +0100

    dak-setup: use `set -e` & friends
    Previously errors in `dak-setup` such as `dak update-db` failing to
    apply a database update would get ignored by the CI system.

--- setup/dak-setup.sh
+++ setup/dak-setup.sh
@@ -17,6 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+set -e
+set -u
+set -o pipefail
 
 dak-setup() {
   # Get the parent directory of the current script
