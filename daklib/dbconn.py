Debian/dak

commit 7842dab81f65056131abfd27b169a2ac2cc47172

implement opening connection to LDAP only once
Also import the ldap module only when needed.  This allows using dak
without the Python ldap module installed.

daklib/dbconn.py
@@ -918,22 +918,9 @@ class Keyring(object):
             raise GpgException("command failed: %s\nstdout: %s\nstderr: %s\n" % (cmd, out, err))
 
     def import_users_from_ldap(self, session):
-        import ldap
+        from .utils import open_ldap_connection
+        l = open_ldap_connection()
         cnf = Config()
-
-        LDAPDn = cnf["Import-LDAP-Fingerprints::LDAPDn"]
-        LDAPServer = cnf["Import-LDAP-Fingerprints::LDAPServer"]
-        ca_cert_file = cnf.get('Import-LDAP-Fingerprints::CACertFile')
-
-        l = ldap.initialize(LDAPServer)
-
-        if ca_cert_file:
-            l.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_HARD)
-            l.set_option(ldap.OPT_X_TLS_CACERTFILE, ca_cert_file)
-            l.set_option(ldap.OPT_X_TLS_NEWCTX, True)
-            l.start_tls_s()
-
-        l.simple_bind_s("", "")
         Attrs = l.search_s(LDAPDn, ldap.SCOPE_ONELEVEL,
                "(&(keyfingerprint=*)(supplementaryGid=%s))" % (cnf["Import-Users-From-Passwd::ValidGID"]),
                ["uid", "keyfingerprint", "cn", "mn", "sn"])

daklib/utils.py
@@ -37,7 +37,6 @@ import apt_pkg
 import re
 import email as modemail
 import subprocess
-import ldap
 import errno
 import functools
 import six
@@ -773,13 +772,32 @@ def gpg_get_key_addresses(fingerprint):
 ################################################################################
 
 
+def open_ldap_connection():
+    """open connection to the configured LDAP server"""
+    import ldap
+
+    LDAPDn = Cnf["Import-LDAP-Fingerprints::LDAPDn"]
+    LDAPServer = Cnf["Import-LDAP-Fingerprints::LDAPServer"]
+    ca_cert_file = Cnf.get('Import-LDAP-Fingerprints::CACertFile')
+
+    l = ldap.initialize(LDAPServer)
+
+    if ca_cert_file:
+        l.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_HARD)
+        l.set_option(ldap.OPT_X_TLS_CACERTFILE, ca_cert_file)
+        l.set_option(ldap.OPT_X_TLS_NEWCTX, True)
+        l.start_tls_s()
+
+    l.simple_bind_s("", "")
+
+    return l
+
+################################################################################
+
+
 def get_logins_from_ldap(fingerprint='*'):
     """retrieve login from LDAP linked to a given fingerprint"""
-
-    LDAPDn = Cnf['Import-LDAP-Fingerprints::LDAPDn']
-    LDAPServer = Cnf['Import-LDAP-Fingerprints::LDAPServer']
-    l = ldap.initialize(LDAPServer)
-    l.simple_bind_s('', '')
+    l = open_ldap_connection()
     Attrs = l.search_s(LDAPDn, ldap.SCOPE_ONELEVEL,
                        '(keyfingerprint=%s)' % fingerprint,
                        ['uid', 'keyfingerprint'])
@@ -795,11 +813,7 @@ def get_logins_from_ldap(fingerprint='*'):
 
 def get_users_from_ldap():
     """retrieve login and user names from LDAP"""
-
-    LDAPDn = Cnf['Import-LDAP-Fingerprints::LDAPDn']
-    LDAPServer = Cnf['Import-LDAP-Fingerprints::LDAPServer']
-    l = ldap.initialize(LDAPServer)
-    l.simple_bind_s('', '')
+    l = open_ldap_connection()
     Attrs = l.search_s(LDAPDn, ldap.SCOPE_ONELEVEL,
                        '(uid=*)', ['uid', 'cn', 'mn', 'sn'])
     users = {}

commit 369f2fc744e60fee7680d3a1ed29477a759cf0d3

restore `LDAPDn` local variable
Regression introduced in 7842dab81f65056131abfd27b169a2ac2cc47172

daklib/dbconn.py
@@ -921,6 +921,7 @@ class Keyring(object):
         from .utils import open_ldap_connection
         l = open_ldap_connection()
         cnf = Config()
+        LDAPDn = cnf["Import-LDAP-Fingerprints::LDAPDn"]
         Attrs = l.search_s(LDAPDn, ldap.SCOPE_ONELEVEL,
                "(&(keyfingerprint=*)(supplementaryGid=%s))" % (cnf["Import-Users-From-Passwd::ValidGID"]),
                ["uid", "keyfingerprint", "cn", "mn", "sn"])

daklib/utils.py
@@ -798,6 +798,7 @@ def open_ldap_connection():
 def get_logins_from_ldap(fingerprint='*'):
     """retrieve login from LDAP linked to a given fingerprint"""
     l = open_ldap_connection()
+    LDAPDn = Cnf["Import-LDAP-Fingerprints::LDAPDn"]
     Attrs = l.search_s(LDAPDn, ldap.SCOPE_ONELEVEL,
                        '(keyfingerprint=%s)' % fingerprint,
                        ['uid', 'keyfingerprint'])
@@ -814,6 +815,7 @@ def get_logins_from_ldap(fingerprint='*'):
 def get_users_from_ldap():
     """retrieve login and user names from LDAP"""
     l = open_ldap_connection()
+    LDAPDn = Cnf["Import-LDAP-Fingerprints::LDAPDn"]
     Attrs = l.search_s(LDAPDn, ldap.SCOPE_ONELEVEL,
                        '(uid=*)', ['uid', 'cn', 'mn', 'sn'])
     users = {}