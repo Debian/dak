80e63881adb19bdc9ec14d283b8def48f9e5278a
Ansgar <ansgar@debian.org>
2020-12-02 20:28:33 +0100

    typo: to → do

--- scripts/debian/dep11-basic-validate.py
+++ scripts/debian/dep11-basic-validate.py
@@ -117,7 +117,7 @@ def validate_data(data):
             continue
         if not pkgname:
             if doc.get('Merge'):
-                # merge instructions to not need a package name
+                # merge instructions do not need a package name
                 continue
             if cpttype not in ['web-application', 'operating-system', 'repository']:
                 add_issue('[%s]: %s' % (cptid, 'Component is missing a \'Package\' key.'))
