Debian/dak

commit 84bb48818a0248f37556c4432ffcb46f0d7f246e

Replace instances of ``if x.has_key(y)`` with ``if y in x``.

dak/rm.py
@@ -140,9 +140,10 @@ def main ():
                "architecture", "binary", "binary-only", "carbon-copy", "component",
                "done", "help", "no-action", "partial", "rdep-check", "reason",
                "source-only", "Do-Close" ]:
-        if not cnf.has_key("Rm::Options::%s" % (i)):
-            cnf["Rm::Options::%s" % (i)] = ""
-    if not cnf.has_key("Rm::Options::Suite"):
+        key = "Rm::Options::%s" % (i)
+        if key not in cnf:
+            cnf[key] = ""
+    if "Rm::Options::Suite" not in cnf:
         cnf["Rm::Options::Suite"] = "unstable"
 
     arguments = apt_pkg.parse_commandline(cnf.Cnf, Arguments, sys.argv)
@@ -162,7 +163,7 @@ def main ():
             or (Options["Binary"] and Options["Binary-Only"])
             or (Options["Binary-Only"] and Options["Source-Only"])):
         utils.fubar("Only one of -b/--binary, -B/--binary-only and -S/--source-only can be used.")
-    if Options.has_key("Carbon-Copy") and not Options.has_key("Done"):
+    if "Carbon-Copy" not in Options and "Done" not in Options:
         utils.fubar("can't use -C/--carbon-copy without also using -d/--done option.")
     if Options["Architecture"] and not Options["Partial"]:
         utils.warn("-a/--architecture implies -p/--partial.")
@@ -288,13 +289,13 @@ def main ():
     carbon_copy = []
     for copy_to in utils.split_args(Options.get("Carbon-Copy")):
         if copy_to.isdigit():
-            if cnf.has_key("Dinstall::BugServer"):
+            if "Dinstall::BugServer" in cnf:
                 carbon_copy.append(copy_to + "@" + cnf["Dinstall::BugServer"])
             else:
                 utils.fubar("Asked to send mail to #%s in BTS but Dinstall::BugServer is not configured" % copy_to)
         elif copy_to == 'package':
             for package in set([s[5] for s in to_remove]):
-                if cnf.has_key("Dinstall::PackagesServer"):
+                if "Dinstall::PackagesServer" in cnf:
                     carbon_copy.append(package + "@" + cnf["Dinstall::PackagesServer"])
         elif '@' in copy_to:
             carbon_copy.append(copy_to)
@@ -323,9 +324,9 @@ def main ():
         architecture = i[2]
         maintainer = i[4]
         maintainers[maintainer] = ""
-        if not d.has_key(package):
+        if package not in d:
             d[package] = {}
-        if not d[package].has_key(version):
+        if version not in d[package]:
             d[package][version] = []
         if architecture not in d[package][version]:
             d[package][version].append(architecture)

dak/add_user.py
@@ -55,8 +55,9 @@ def main():
                  ]
 
     for i in [ "help" ]:
-        if not Cnf.has_key("Add-User::Options::%s" % (i)):
-            Cnf["Add-User::Options::%s" % (i)] = ""
+        key = "Add-User::Options::%s" % i
+        if key not in Cnf:
+            Cnf[key] = ""
 
     apt_pkg.parse_commandline(Cnf, Arguments, sys.argv)
 
@@ -83,7 +84,7 @@ def main():
     primary_key = primary_key.replace(" ","")
 
     uid = ""
-    if Cnf.has_key("Add-User::Options::User") and Cnf["Add-User::Options::User"]:
+    if "Add-User::Options::User" in Cnf and Cnf["Add-User::Options::User"]:
         uid = Cnf["Add-User::Options::User"]
         name = Cnf["Add-User::Options::User"]
     else:
@@ -120,7 +121,7 @@ def main():
         session.commit()
 
         # Lets add user to the email-whitelist file if its configured.
-        if Cnf.has_key("Dinstall::MailWhiteList") and Cnf["Dinstall::MailWhiteList"] != "":
+        if "Dinstall::MailWhiteList" in Cnf and Cnf["Dinstall::MailWhiteList"] != "":
             f = utils.open_file(Cnf["Dinstall::MailWhiteList"], "a")
             for mail in emails:
                 f.write(mail+'\n')

dak/admin.py
@@ -836,13 +836,13 @@ def show_config(command):
 
     if mode == 'db':
         connstr = ""
-        if cnf.has_key("DB::Service"):
+        if "DB::Service" in cnf:
             # Service mode
             connstr = "postgresql://service=%s" % cnf["DB::Service"]
-        elif cnf.has_key("DB::Host"):
+        elif "DB::Host" in cnf:
             # TCP/IP
             connstr = "postgres://%s" % cnf["DB::Host"]
-            if cnf.has_key("DB::Port") and cnf["DB::Port"] != "-1":
+            if "DB::Port" in cnf and cnf["DB::Port"] != "-1":
                 connstr += ":%s" % cnf["DB::Port"]
             connstr += "/%s" % cnf["DB::Name"]
         else:
@@ -853,16 +853,16 @@ def show_config(command):
         print connstr
     elif mode == 'db-shell':
         e = []
-        if cnf.has_key("DB::Service"):
+        if "DB::Service" in cnf:
             e.append('PGSERVICE')
             print "PGSERVICE=%s" % cnf["DB::Service"]
-        if cnf.has_key("DB::Name"):
+        if "DB::Name" in cnf:
             e.append('PGDATABASE')
             print "PGDATABASE=%s" % cnf["DB::Name"]
-        if cnf.has_key("DB::Host"):
+        if "DB::Host" in cnf:
             print "PGHOST=%s" % cnf["DB::Host"]
             e.append('PGHOST')
-        if cnf.has_key("DB::Port") and cnf["DB::Port"] != "-1":
+        if "DB::Port" in cnf and cnf["DB::Port"] != "-1":
             print "PGPORT=%s" % cnf["DB::Port"]
             e.append('PGPORT')
         print "export " + " ".join(e)
@@ -1018,8 +1018,9 @@ def main():
     arguments = [('h', "help", "Admin::Options::Help"),
                  ('n', "dry-run", "Admin::Options::Dry-Run")]
     for i in [ "help", "dry-run" ]:
-        if not Cnf.has_key("Admin::Options::%s" % (i)):
-            Cnf["Admin::Options::%s" % (i)] = ""
+        key = "Admin::Options::%s" % i
+        if key not in Cnf:
+            Cnf[key] = ""
 
     arguments = apt_pkg.parse_commandline(Cnf, arguments, sys.argv)
 

dak/archive_dedup_pool.py
@@ -124,8 +124,9 @@ def main():
     apt_pkg.parse_commandline(cnf.Cnf,Arguments,sys.argv)
 
     for i in ["help"]:
-        if not cnf.has_key("Archive-Dedup-Pool::Options::%s" % (i)):
-            cnf["Archive-Dedup-Pool::Options::%s" % (i)] = ""
+        key = "Archive-Dedup-Pool::Options::%s" % i
+        if key not in cnf:
+            cnf[key] = ""
 
     Options = cnf.subtree("Archive-Dedup-Pool::Options")
 

dak/auto_decruft.py
@@ -426,8 +426,9 @@ def main ():
                  ("\0",'decruft-equal-versions',"Auto-Decruft::Options::OtherSuiteDecruftEqual")
                 ]
     for i in ["help", "Dry-Run", "Debug", "OtherSuite", "OtherSuiteRMMsg", "OtherSuiteDecruftEqual"]:
-        if not cnf.has_key("Auto-Decruft::Options::%s" % (i)):
-            cnf["Auto-Decruft::Options::%s" % (i)] = ""
+        key = "Auto-Decruft::Options::%s" % i
+        if key not in cnf:
+            cnf[key] = ""
 
     cnf["Auto-Decruft::Options::Suite"] = cnf.get("Dinstall::DefaultSuite", "unstable")
 

dak/bts_categorize.py
@@ -159,7 +159,7 @@ def main():
 
     for arg in arguments:
         opt = "BtsCategorize::Options::%s" % arg[1]
-        if not Cnf.has_key(opt):
+        if opt not in Cnf:
             Cnf[opt] = ""
 
     packages = apt_pkg.parse_commandline(Cnf, arguments, sys.argv)

dak/check_archive.py
@@ -98,7 +98,7 @@ def process_dir (unused, dirname, filenames):
         return
     for name in filenames:
         filename = os.path.abspath(os.path.join(dirname,name))
-        if os.path.isfile(filename) and not os.path.islink(filename) and not db_files.has_key(filename) and not excluded.has_key(filename):
+        if os.path.isfile(filename) and not os.path.islink(filename) and filename not in db_files and filename not in excluded:
             waste += os.stat(filename)[stat.ST_SIZE]
             print "%s" % (filename)
 
@@ -532,8 +532,9 @@ def main ():
 
     Arguments = [('h',"help","Check-Archive::Options::Help")]
     for i in [ "help" ]:
-        if not cnf.has_key("Check-Archive::Options::%s" % (i)):
-            cnf["Check-Archive::Options::%s" % (i)] = ""
+        key = "Check-Archive::Options::%s" % i
+        if key not in cnf:
+            cnf[key] = ""
 
     args = apt_pkg.parse_commandline(cnf.Cnf, Arguments, sys.argv)
 

dak/check_overrides.py
@@ -148,10 +148,10 @@ SELECT s.source FROM source s
     if otype == "dsc":
         for i in q.fetchall():
             package = i[0]
-            if src_packages.has_key(package):
+            if package in src_packages:
                 src_packages[package] = 1
             else:
-                if blacklist.has_key(package):
+                if package in blacklist:
                     utils.warn("%s in incoming, not touching" % package)
                     continue
                 Logger.log(["removing unused override", osuite, component,
@@ -170,7 +170,7 @@ SELECT s.source FROM source s
                             {'suite_id': osuite_id, 'component_id': component_id})
         for i in q.fetchall():
             package = i[0]
-            if not src_packages.has_key(package) or src_packages[package]:
+            if package not in src_packages or src_packages[package]:
                 continue
             src_packages[package] = 1
 
@@ -202,7 +202,7 @@ SELECT s.source FROM source s
                                  'component_id': component_id, 'type_id': type_id})
             for i in q.fetchall():
                 package = i[0]
-                if not src_packages.has_key(package) or src_packages[package]:
+                if package not in src_packages or src_packages[package]:
                     if i[4] and (i[1] != i[4] or i[2] != i[5] or i[3] != i[6]):
                         Logger.log(["syncing override", osuite, component,
                             otype, package, "source", sections[i[5]], i[6], "source", sections[i[2]], i[3]])
@@ -240,10 +240,10 @@ SELECT s.source FROM source s
     else: # binary override
         for i in q.fetchall():
             package = i[0]
-            if packages.has_key(package):
+            if package in packages:
                 packages[package] = 1
             else:
-                if blacklist.has_key(package):
+                if package in blacklist:
                     utils.warn("%s in incoming, not touching" % package)
                     continue
                 Logger.log(["removing unused override", osuite, component,
@@ -274,7 +274,7 @@ SELECT s.source FROM source s
                                   'component_id': component_id, 'type_id': type_id})
             for i in q.fetchall():
                 package = i[0]
-                if not packages.has_key(package) or packages[package]:
+                if package not in packages or packages[package]:
                     if i[4] and (i[1] != i[4] or i[2] != i[5] or i[3] != i[6]):
                         Logger.log(["syncing override", osuite, component,
                             otype, package, priorities[i[4]], sections[i[5]],
@@ -324,8 +324,9 @@ def main ():
     Arguments = [('h',"help","Check-Overrides::Options::Help"),
                  ('n',"no-action", "Check-Overrides::Options::No-Action")]
     for i in [ "help", "no-action" ]:
-        if not cnf.has_key("Check-Overrides::Options::%s" % (i)):
-            cnf["Check-Overrides::Options::%s" % (i)] = ""
+        key = "Check-Overrides::Options::%s" % i
+        if key not in cnf:
+            cnf[key] = ""
     apt_pkg.parse_commandline(cnf.Cnf, Arguments, sys.argv)
     Options = cnf.subtree("Check-Overrides::Options")
 

commit 5bc5fc20b7f7ceb7b20fa6af560174c2149ff361

dak/rm.py: remove superfluous check for `--carbon-copy` or `--done`
The check for either `--carbon-copy` or `--done` is performed a bit
later with a correct error message if neither is specified:

  Need a -C/--carbon-copy if not closing a bug and not doing a cruft removal.

The check removed here originally checked that `--carbon-copy` was
always used together with `--done` (as the error message implied), but
the condition was unintentionally broken in
84bb48818a0248f37556c4432ffcb46f0d7f246e.

Use of `--carbon-copy` without `--done` seems useful and desired (per
the error above), so just remove the incorrect check here.

dak/rm.py
@@ -165,8 +165,6 @@ def main():
     nr_actions = len([act for act in actions if act])
     if nr_actions > 1:
         utils.fubar("Only one of -b/--binary, -B/--binary-only and -S/--source-only can be used.")
-    if "Carbon-Copy" not in Options and "Done" not in Options:
-        utils.fubar("can't use -C/--carbon-copy without also using -d/--done option.")
     if Options["Architecture"] and not Options["Partial"]:
         utils.warn("-a/--architecture implies -p/--partial.")
         Options["Partial"] = "true"