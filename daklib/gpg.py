Debian/dak

commit d438328783599a74b903272e7eab4e33cdbb53bc

gpg.py: allow exit code 2

daklib/gpg.py
@@ -151,7 +151,11 @@ class SignedFile:
                     stderr = self.stderr.decode('ascii', errors='replace')
                     raise GpgException("No status output from GPG. (GPG exited with status code %s)\n%s" % (exit_code, stderr))
 
-                if exit_code not in (0, 1):
+                # gpg exits with 0 (no error), 1 (at least one invalid sig),
+                # or anything else (fatal error).  Even though status 2
+                # indicates a fatal error, we still allow it as it is also
+                # returned when the public key is not known.
+                if exit_code not in (0, 1, 2):
                     stderr = self.stderr.decode('ascii', errors='replace')
                     raise GpgException(f"GPG exited with a fatal error (exit code {exit_code})\n{stderr}")
 

commit b818ce33057794f3ade7819cfa6db7bb564f29e0

gpg.py: exit with status code `3` when failing to exec gpg
With commit d438328783599a74b903272e7eab4e33cdbb53bc we do no longer
treat exit code 2 as a fatal error. But failure to run gpg should be a
fatal error.

daklib/gpg.py
@@ -315,7 +315,7 @@ class SignedFile:
             except:
                 # Ignore errors, we want to reach the `exit` call below.
                 pass
-            os._exit(2)
+            os._exit(3)
 
     def contents_sha1(self):
         return apt_pkg.sha1sum(self.contents)